f60ed7b47fbfc7b81a6cdaf963a12d01f0706bb4
Rustam Aktuganov <aktuganov@yandex.ru>
2021-07-28 14:42:51 +0200

    initial commit

---
+++ src/main/java/ru/netology/SpringBootConditional/SpringBootConditionalApplication.java
@@ -0,0 +1,13 @@
+package ru.netology.SpringBootConditional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConditionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootConditionalApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ru/netology/SpringBootConditional/SystemProfile.java
@@ -0,0 +1,5 @@
+package ru.netology.SpringBootConditional;
+
+public interface SystemProfile {
+    String getProfile();
+}
